30db473cc81f431155ef7a55fcef33c0b263e9c1
rohit5krish <rohitkrishnark5@gmail.com>
2022-08-16 11:19:45 +0530

    Google SignUp Button

--- lib/presentation/login/widget/google_signin_button.dart
+++ lib/presentation/login/widget/google_signin_button.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:social_media/application/signup/signup_bloc.dart';
-import 'package:social_media/core/constants/colors.dart';
-import 'package:social_media/core/constants/styles.dart';
 import 'package:social_media/infrastructure/auth_methods/auth_methods.dart';
 
 class GoogleSignInButton extends StatelessWidget {
@@ -14,29 +10,13 @@ class GoogleSignInButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () async {
-        // BlocProvider.of<SignupBloc>(context).add(GoogleLogin());
         await AuthMethods().GoogleLogin();
       },
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        width: double.infinity,
-        height: 45,
-        decoration: BoxDecoration(
-            color: blueClr800, borderRadius: BorderRadius.circular(10)),
-        child: Row(
-          children: [
-            Image.asset(
-              'assets/googleLogo1.png',
-              height: 20,
-              width: 20,
-            ),
-            const Spacer(),
-            const Text(
-              'Sign in Google account',
-              style: whiteTxt15,
-            ),
-            const Spacer()
-          ],
+      child: ClipRect(
+        child: Image.asset(
+          'assets/googleLogo1.png',
+          height: 40,
+          width: 40,
         ),
       ),
     );
