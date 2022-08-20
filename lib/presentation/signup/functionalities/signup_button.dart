bd05d376fea1ec8290e292e9989341981f7fb02a
rohit5krish <rohitkrishnark5@gmail.com>
2022-08-20 16:39:44 +0530

    OTP functionality disabled temporarily

--- lib/presentation/signup/functionalities/signup_button.dart
+++ lib/presentation/signup/functionalities/signup_button.dart
@@ -18,33 +18,29 @@ Future<void> SignupButtonClicked({
   if (isValid) {
     BlocProvider.of<SignupBloc>(context).add(UpdateLoading(isLoad: true));
 
-    String result = await AuthMethods().validateEmail(signupDetails.email);
+    // String result = await AuthMethods().validateEmail(signupDetails.email);
 
-    // String res = await AuthMethods().SignUpUser(
-    //   username: username,
-    //   email: email,
-    //   password: password,
-    //   profileImage: profImage,
-    // );
+    String result = await AuthMethods().SignUpUser(
+      username: signupDetails.username,
+      email: signupDetails.email,
+      password: signupDetails.password,
+      profileImage: signupDetails.profImage,
+    );
     BlocProvider.of<SignupBloc>(context).add(UpdateLoading(isLoad: false));
     if (result == 'success') {
-      String otpResult =
-          await AuthMethods().sendOtp(email: signupDetails.email);
-      if (otpResult == 'success') {
-        Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-          return SignUpInheritedWidget(
-              child: OtpPage(), signupDetails: signupDetails);
-        }));
-        showSnackBar('Otp Sent Successfully', context);
-      } else {
-        showSnackBar(otpResult, context);
-      }
+      // String otpResult =
+      //     await AuthMethods().sendOtp(email: signupDetails.email);
+      // if (otpResult == 'success') {
+      //   Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+      //     return SignUpInheritedWidget(
+      //         child: OtpPage(), signupDetails: signupDetails);
+      //   }));
 
-      // Navigator.of(context).pushAndRemoveUntil(
-      //     MaterialPageRoute(builder: (context) {
-      //   return NavigationPage();
-      // }), (route) => false);
-
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(builder: (context) {
+        return NavigationPage();
+      }), (route) => false);
+      showSnackBar('Sign Up Successfully', context);
     } else if (signupDetails.username.isNotEmpty &&
         signupDetails.password.isNotEmpty &&
         signupDetails.email.isNotEmpty) {
